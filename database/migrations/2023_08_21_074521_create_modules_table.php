f5b46b32549a5b298f6af3686c259b91016e8902
Azzasafah <muhammad.hafizh2016@gmail.com>
2023-08-21 14:56:12 +0700

    Develop model donation

---
+++ database/migrations/2023_08_21_074521_create_modules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('modules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('course_id');
+            $table->string('title');
+            $table->text('description');
+            $table->enum('module_type', array(
+                'file','video'
+            ));
+            $table->string('file_type')->nullable();
+            $table->string('video')->nullable();
+            $table->string('document')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('modules');
+    }
+};
